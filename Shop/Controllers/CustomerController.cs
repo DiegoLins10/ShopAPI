5c0e3e528fae65fcf0a012626ff832a523580138
DiegoLins10 <diegofernandeslins@gmail.com>
2024-10-14 21:43:32 -0300

    Criação da controller customer

---
+++ Shop/Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+using Shop.Domain.Handlers;
+
+namespace Shop.Controllers
+{
+    [Route("[controller]/v1/")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpPost]  
+        public CreateCustomerResponse Create([FromServices] ICreateCustomerHandler handler, [FromBody] CreateCustomerRequest command)
+        {
+            return handler.Handle(command);
+        }
+    }
+}
