18413ae752a0b20b9f204510658890ed67e694c7
DiegoLins10 <diegofernandeslins@gmail.com>
2024-10-14 21:42:58 -0300

    Criação de handler

---
+++ Shop/Domain/Handlers/CreateCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿using Shop.Domain.Commands.Requests;
+using Shop.Domain.Commands.Responses;
+
+namespace Shop.Domain.Handlers
+{
+    public class CreateCustomerHandler : ICreateCustomerHandler
+    {
+        public CreateCustomerResponse Handle(CreateCustomerRequest request)
+        {
+            return new CreateCustomerResponse
+            {
+                Id = Guid.NewGuid(),
+                Name = "Diego Lins",
+                Email = "diegolins@ibm.com",
+                Date = DateTime.Now
+            };
+        }
+    }
+}
