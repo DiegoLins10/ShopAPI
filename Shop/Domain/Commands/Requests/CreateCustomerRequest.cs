748b41b9b2d7152cbbcbc85e97096c3afc07aafe
DiegoLins10 <diegofernandeslins@gmail.com>
2024-10-14 21:55:02 -0300

    Alteração classe request

--- Shop/Domain/Commands/Requests/CreateCustomerRequest.cs
+++ Shop/Domain/Commands/Requests/CreateCustomerRequest.cs
@@ -2,9 +2,7 @@
 {
     public class CreateCustomerRequest
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime Date { get; set; }
     }
 }
